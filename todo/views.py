a648dc22b3d72f4b50d23a26855e6d119b319d9c
Ahmad34-90 <ahmiiikha@gmail.com>
2026-02-18 15:04:29 +0500

    Task creation and dispaly them.

--- todo/views.py
+++ todo/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
-
-# Create your views here.
+from django.shortcuts import render, redirect
+from .models import ToDo
+def todo(request):
+    todo= ToDo.objects.all()
+    
+    if request.method == "POST":
+        if "add_task" in request.POST:
+            title = request.POST.get("title")
+            if title:
+                ToDo.objects.create(title=title)
+            return redirect("todo")
+            
+    return render(request, "todo.html",{"todo":todo})
