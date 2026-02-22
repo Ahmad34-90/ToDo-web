fdda9e0bc0aa88eb5beeccc8275fc157a2b1a66b
Ahmad34-90 <ahmiiikha@gmail.com>
2026-02-22 21:37:48 +0500

    Adding Task update and improving style.

--- todo/urls.py
+++ todo/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.todo, name="todo"),
+    path("", views.todo_list, name="todo"),
+    path("delete_task/<int:id>/", views.delete, name="delete"),
+    path("update_task/<int:id>/",views.update, name="update"),
 ]
--- todo/views.py
+++ todo/views.py
@@ -1,13 +1,28 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from .models import ToDo
-def todo(request):
-    todo= ToDo.objects.all()
-    
+
+def todo_list(request):
+    tasks = ToDo.objects.all()
     if request.method == "POST":
-        if "add_task" in request.POST:
             title = request.POST.get("title")
             if title:
-                ToDo.objects.create(title=title)
+                ToDo.objects.create(title = title)
             return redirect("todo")
-            
-    return render(request, "todo.html",{"todo":todo})
+    return render(request, "todo.html", {"tasks":tasks})
+
+
+def delete(request, id):
+    if request.method== "POST":
+        tasks = get_object_or_404(ToDo, id = id)
+        tasks.delete()
+    return redirect("todo")
+
+def update(request, id):
+    tasks = get_object_or_404(ToDo, id=id)
+    if request.method =="POST":
+        new_title = request.POST.get("title")
+        if new_title:
+            tasks.title = new_title
+            tasks.save()
+        return redirect("todo")
+    return render(request,"update.html",{"tasks":tasks})
